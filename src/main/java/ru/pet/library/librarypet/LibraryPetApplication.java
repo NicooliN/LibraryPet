cc8c2d9e553ccad83bc2f6177caead982bf71887
Niko91 <Niko91@tpu.ru>
2023-09-21 19:34:19 +0300

    initial

---
+++ src/main/java/ru/pet/library/librarypet/LibraryPetApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.library.librarypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pet/library/librarypet/LibraryPetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.library.librarypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
